287b6e1a10472496e4df6b69051a0068c902d9c4
Joseph Kanner <josephkanner@gmail.com>
2019-06-23 19:23:44 +0300

    form

--- app/models/sock.rb
+++ app/models/sock.rb
@@ -1,12 +1,12 @@
 class Sock < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
-  validates :color, presense: true
-  validates :price, presense: true, numericality: true
+  validates :color, presence: true
+  validates :price, presence: true, numericality: true
   validates :pattern, collection: ["Solid", "Striped", "Polko dots", "Other"], allow_blank: true
   validates :min_size, greater_than_or_equal_to: 0, allow_blank: true
   validates :max_size, less_than_or_equal_to: 50, allow_blank: true
-  validates :description, presense: true, allow_blank: true
+  validates :description, presence: true, allow_blank: true
   validates :sock_type, collection: ["Over the knee", "Knee high", "Over the calf/Executive", "Mid-calf/Crew", "Quarter/Anklets", "Ped/Low cut", "Liner, Extra low cut", "Invisible/No-show", "Toe cover/Mute"], allow_blank: true
   validates :brand, allow_blank: true
   validates :season, collection: ["spring", "winter", "summer", "autumn"], allow_blank: true
