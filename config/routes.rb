b163b6d7190aaca23257b3ef58dd5d6eed5220e3
(Maximilian Pfadenhauer) <max.pfadenhauer@gmail.com>
2019-06-23 16:36:27 +0300

    added resources and transactions

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'transactions/new'
-  get 'transactions/create'
-  get 'socks/index'
-  get 'socks/show'
-  get 'socks/new'
-  get 'socks/create'
-  get 'socks/edit'
-  get 'socks/update'
-  get 'socks/delete'
+resources :socks do
+  resources :transactions, only: :show
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
