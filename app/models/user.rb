deb1dfbe89c41fd3a6df9074e699c62fc8bb73c5
maya aderka <mayaad0@gmail.com>
2019-06-24 14:51:45 +0300

    added profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :reviews
+  has_many :socks
 
   validates :username, presence: true
 end
