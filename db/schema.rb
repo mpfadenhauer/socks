3b9208eade5c9ffe21d686a1358f8f1f8fe89088
Joseph Kanner <josephkanner@gmail.com>
2019-06-24 08:58:47 +0300

    not important - disgard

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_23_125238) do
+ActiveRecord::Schema.define(version: 2019_06_23_125704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "description"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "socks", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "color"
+    t.string "pattern"
+    t.integer "min_size"
+    t.integer "max_size"
+    t.string "type"
+    t.string "brand"
+    t.string "season"
+    t.integer "age"
+    t.float "price"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_socks_on_user_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.string "type"
+    t.bigint "user_id"
+    t.bigint "sock_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["sock_id"], name: "index_transactions_on_sock_id"
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +64,8 @@ ActiveRecord::Schema.define(version: 2019_06_23_125238) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reviews", "users"
+  add_foreign_key "socks", "users"
+  add_foreign_key "transactions", "socks"
+  add_foreign_key "transactions", "users"
 end
