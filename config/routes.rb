9dd655b74d285484fc59a3ba680d0ea52c996f7c
Charlotte Hetzler <charlotte@hetzler.de>
2019-06-24 11:57:21 +0300

    change style + add user links

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-resources :socks do
-  resources :transactions, only: [:show, :new, :create]
   devise_for :users
-end
+  resources :socks do
+    resources :transactions, only: [:show, :new, :create]
+  end
   root to: 'socks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
