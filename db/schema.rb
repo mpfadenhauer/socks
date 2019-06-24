6286e8819020e2b664403a3ad9329de479f8879a
Joseph Kanner <josephkanner@gmail.com>
2019-06-24 15:47:43 +0300

    buy a sock

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_24_093109) do
+ActiveRecord::Schema.define(version: 2019_06_24_121236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_06_24_093109) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "bought", default: false
     t.index ["user_id"], name: "index_socks_on_user_id"
   end
 
