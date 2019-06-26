8fa5a4ffad27183759764e2a37617461495b65d5
maya aderka <mayaad0@gmail.com>
2019-06-26 12:45:44 +0300

    fixed sweet alert

--- app/javascript/components/sweetalert.js
+++ app/javascript/components/sweetalert.js
@@ -2,17 +2,18 @@ import Swal from 'sweetalert2'
 
 const sockcess = () => {
   const button = document.querySelector("#sockcess")
-
-  button.addEventListener("click", (event) => {
-   Swal.fire({
-          title: 'SockCess!',
-          html:'Sock transaction complete 🧦, <br>' +
-          'One small sock for man, one giant sock for mankind,<br> <br> ' +
-          'View order details <a href= "#"> here</a>',
-          type: 'success',
-          showCloseButton: true
-        });
-  });
+  if (button) {
+    button.addEventListener("click", (event) => {
+     Swal.fire({
+            title: 'SockCess!',
+            html:'Sock transaction complete 🧦, <br>' +
+            'One small sock for man, one giant sock for mankind,<br> <br> ' +
+            'View order details <a href= "#"> here</a>',
+            type: 'success',
+            showCloseButton: true
+          });
+    });
+  }
 }
 
 
